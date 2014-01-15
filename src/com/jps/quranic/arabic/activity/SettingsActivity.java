7464cf95d1b6a472a8c562fe65b6c56fc23119fe
Shah <admin@Rameezs-MacBook-Air.local>
2014-01-14 17:11:32 -0800

    Initial work on saving settings

--- src/com/jps/quranic/arabic/activity/SettingsActivity.java
+++ src/com/jps/quranic/arabic/activity/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.jps.quranic.arabic.activity;
 
 import android.app.ListActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
@@ -8,6 +9,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.jps.quranic.arabic.R;
 
@@ -18,6 +21,9 @@ import com.jps.quranic.arabic.R;
  */
 public class SettingsActivity extends ListActivity
 {
+  public static final String SETTINGS_PREF = "settings_pref";
+  public static final String KEY_CHECKED_LESSON_SET = "checkedLessonSet";
+
   @Override
   protected void onCreate( Bundle savedInstanceState )
   {
@@ -46,5 +52,39 @@ public class SettingsActivity extends ListActivity
     ArrayAdapter<String> adapter;
     adapter = new ArrayAdapter<String>( this, android.R.layout.simple_list_item_multiple_choice, lessonList );
     setListAdapter( adapter );
+
+    // get checked lessons from preferences
+    SharedPreferences prefs = getSharedPreferences( SETTINGS_PREF, 0 );
+    Set<String> checkedLessonSet = prefs.getStringSet( KEY_CHECKED_LESSON_SET, new HashSet<String>() );
+
+    if ( !checkedLessonSet.isEmpty() )
+    {
+      for ( String checkedLesson : checkedLessonSet )
+      {
+        listView.setItemChecked( Integer.valueOf( checkedLesson ), true );
+      }
+    }
+  }
+
+  @Override
+  protected void onStop()
+  {
+    super.onStop();
+
+    ListView listView = getListView();
+    Set<String> checkedLessonSet = new HashSet<String>();
+    for ( int i = 0; i < listView.getCount(); i++ )
+    {
+      if ( listView.isItemChecked( i ) )
+      {
+        checkedLessonSet.add( String.valueOf( i ) );
+      }
+    }
+
+    // save checked lessons in preferences
+    SharedPreferences prefs = getSharedPreferences( SETTINGS_PREF, 0 );
+    SharedPreferences.Editor editor = prefs.edit();
+    editor.putStringSet( KEY_CHECKED_LESSON_SET, checkedLessonSet );
+    editor.commit();
   }
 }

3c4f58542f0acabc0d83566c6e7a1ae91fea6dff
Shah <admin@Rameezs-MacBook-Air.local>
2014-04-15 16:46:55 -0700

    'baab's lessons are now displayed with bold and red color

--- src/com/jps/quranic/arabic/adapter/HomeArrayAdapter.java
+++ src/com/jps/quranic/arabic/adapter/HomeArrayAdapter.java
@@ -1,6 +1,7 @@
 package com.jps.quranic.arabic.adapter;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +22,8 @@ public class HomeArrayAdapter extends ArrayAdapter<Lesson>
 {
   private final Context _context;
   private final ArrayList<Lesson> _lessonList;
+  private final int _redColor;
+  private final int _blackColor;
 
   public HomeArrayAdapter( Context context, ArrayList<Lesson> lessonList )
   {
@@ -28,6 +31,8 @@ public class HomeArrayAdapter extends ArrayAdapter<Lesson>
 
     _context = context;
     _lessonList = lessonList;
+    _redColor = _context.getResources().getColor( android.R.color.holo_red_light );
+    _blackColor = _context.getResources().getColor( android.R.color.black );
   }
 
   @Override
@@ -44,8 +49,27 @@ public class HomeArrayAdapter extends ArrayAdapter<Lesson>
     TextView lessonTitleView = (TextView) view.findViewById( R.id.lesson_title );
 
     // 4. Set the text for textView
+    final String lessonTitle = _lessonList.get( position ).getLessonTitle();
     lessonNumberView.setText( _lessonList.get( position ).getLessonNumber() );
-    lessonTitleView.setText( _lessonList.get( position ).getLessonTitle() );
+    lessonTitleView.setText( lessonTitle );
+
+    // display 'baab' lessons as bold and red in color
+    if ( lessonTitle != null && lessonTitle.contains( "باب" ) )
+    {
+      lessonNumberView.setTypeface( lessonNumberView.getTypeface(), Typeface.BOLD );
+      lessonNumberView.setTextColor( _redColor );
+
+      lessonTitleView.setTypeface( lessonTitleView.getTypeface(), Typeface.BOLD );
+      lessonTitleView.setTextColor( _redColor );
+    }
+    else
+    {
+      lessonNumberView.setTypeface( lessonNumberView.getTypeface(), Typeface.NORMAL );
+      lessonNumberView.setTextColor( _blackColor );
+
+      lessonTitleView.setTypeface( lessonTitleView.getTypeface(), Typeface.NORMAL );
+      lessonTitleView.setTextColor( _blackColor );
+    }
 
     // 5. return view
     return view;
